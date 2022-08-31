18d104165001d19623652fe5c5fa2fc33c68af08
Daniel Oliveira <drawnpoetry@gmail.com>
2022-08-31 15:15:21 +0100

    feat(doc-guides): add sphinx tabs extension to conf.py
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- source/conf.py
+++ source/conf.py
@@ -29,10 +29,14 @@ release = '0.0.0'
 
 master_doc = 'index'
 
+# enables figures, tables and code-blocks are automatically numbered if they
+# have a caption.
+numfig = True
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxcontrib.spelling']
+extensions = ['sphinxcontrib.spelling', 'sphinx_tabs.tabs']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
